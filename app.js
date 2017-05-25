ba53cd0c33f468df02f94911e862d9f951652804
James <Jayms.Portune@gmail.com>
2017-05-24 21:33:50 -0700

    initial commit with project with scaf

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var confirmed = confirm('Are you ready to rock an roll?!');
+if (confirmed === true) {
+  console.log('The user is ready');
+} else {
+  alert('We are going to go anyway');
+}
